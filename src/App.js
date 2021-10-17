5b40970f60df52decbe481c5277720f978385fea
Nikita Dmitrienko <nikita.d@thrive.io>
2021-10-17 13:03:48 +0300

    Add footer, add tailwind purge

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Footer from './components/Footer';
 import Insult from './components/Insult';
 import './styles/output.css'
 
@@ -6,8 +7,9 @@ import './styles/output.css'
 function App() {
   return (
     <div className="bg-white dark:bg-gray-900 p-20 h-screen flex justify-center items-start flex-col">
-      <h1 className="text-5xl text-gray-800 dark:text-white">Hello 👋</h1>
+      <h1 className="text-4xl text-gray-800 dark:text-white">Hello 👋</h1>
       <Insult />
+      <Footer />
     </div>
   );
 }
